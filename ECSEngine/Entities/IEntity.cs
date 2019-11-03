44dac7ba77604a79be69e9cb9703f16e8db9324a
Alex <alexnoir03@gmail.com>
2019-11-03 23:12:56 +0000

    Changed gitignore to include obj (3d model) files, and added a test mesh + model as content.  Also added a list of components to IEntity - changes are reflected in TestModelEntity.

--- ECSEngine/Entities/IEntity.cs
+++ ECSEngine/Entities/IEntity.cs
@@ -9,6 +9,12 @@ namespace ECSEngine
 {
     public interface IEntity
     {
+        List<IComponent> components { get; set; }
         void HandleEvent(Event eventType, IEventArgs eventArgs);
+
+        IComponent GetComponent<T>(); /* TODO: Consider - this is the same for every entity - it's 
+        just components.Where((t)=>return t.GetType() == typeof(T)) or something like that - so it 
+        might be better practice to somehow make this virtual or move it elsewhere? Leaving it here 
+        for now, just so that it's easier to test, but NEEDS looking at asap. */
     }
 }
--- ECSEngine/Entities/TestModelEntity.cs
+++ ECSEngine/Entities/TestModelEntity.cs
@@ -1,4 +1,5 @@
-﻿using ECSEngine.Events;
+﻿using ECSEngine.Components;
+using ECSEngine.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,28 @@ namespace ECSEngine.Entities
 {
     public class TestModelEntity : IEntity
     {
-        public TestModelEntity() { }
+
+        public List<IComponent> components { get; set; }
+
+        public TestModelEntity()
+        {
+            this.components = new List<IComponent>(); /* TODO: Consider - since this should also (see: IEntity) be 
+            instantiated, also consider moving this elsewhere. (maybe we need those base classes over the 
+            interfaces after all?) */
+
+            // Add mesh component
+            this.components.Add(new MeshComponent("Content/TestMesh.obj"));
+        }
+
+        public IComponent GetComponent<T>()
+        {
+            var matchingComponents = this.components.Where((t) => t.GetType() == typeof(T));
+            if (matchingComponents.Count() > 0) return matchingComponents.First();
+
+            // No match, so throw an exception bigger than the sun because the person who invoked this was an idiot
+            throw new Exception($"Component of type {typeof(T).Name} does not exist on {this.GetType().Name}");
+        }
+
         public void HandleEvent(Event eventType, IEventArgs eventArgs)
         {
             Debug.Log($"Received event {eventType.ToString()}");
